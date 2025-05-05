60fcf5a725b5f0745f72757e27f3e2b59ed6a94a
FaaF-Eng <fabio.teixeira18@hotmail.com>
2025-05-05 00:01:40 -0300

    Password

--- src/containers/fluxo_de_navegacao1/page2.js
+++ src/containers/fluxo_de_navegacao1/page2.js
@@ -1,6 +1,32 @@
+console.log("JavaScript Funciona");
 
-const btn1 = document.getElementById('btn_1');
+// const botao = document.getElementById("meuBotao");
 
-btn1.onclick = function(){
-    console.log("1");
-}
+// botao.addEventListener("click" , function(){
+//     console.log("Meu Botão Funciona");
+// });
+
+
+let senhaDigitada = "";
+const maxCaracteres = 4;
+const campoSenha = document.getElementById("campoSenha");
+
+// Seleciona todos os botões dentro da div grid-botoes
+const botoes = document.querySelectorAll(".grid-botoes button");
+
+botoes.forEach((botao) => {
+  botao.addEventListener("click", () => {
+    const valor = botao.textContent;
+
+    if (valor === "Anula") {
+      senhaDigitada = "";
+      campoSenha.textContent = "";
+      return;
+    }
+
+    if (senhaDigitada.length < maxCaracteres && !isNaN(valor)) {
+      senhaDigitada += "*";
+      campoSenha.textContent = senhaDigitada;
+    }
+  });
+});
