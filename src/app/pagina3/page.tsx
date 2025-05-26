b3539752c1b4f318b19385d84f3e10f1d73d562c
Fabio Teixeira - EIN - TSI <fabio5413@ica.com>
2025-05-26 14:33:47 -0300

    page3 - botões finalizados

--- src/app/pagina3/page.tsx
+++ src/app/pagina3/page.tsx
@@ -14,8 +14,19 @@ export default function page3(){
                     <img src="src=/../veili.png" alt="outralogo" className='mt-100 h-10 z-999 ' />
                 </div>
                 <div className="fixed font-bold mb-55 mr-40">Menu</div>
-                <button className="flex items-center bg-orange-600 hover:bg-orange-700 text-white font-semibold py-2 px-10 rounded-lg transition duration-300">
-                    <img src="src=/../cadastro.png" alt="icone_cadastro" className="w-5 h-5 mr-2" />Cadastro</button>
+                <div id="botoes" className="fixed mt-30 flex flex-col gap-4">
+                    <button className="flex items-center bg-orange-600 hover:bg-orange-700 text-white font-semibold py-2 px-10 rounded-lg transition duration-300">
+                        <img src="src=/../cadastro.png" alt="icone_cadastro" className="w-5 h-5 mr-2" />Cadastro</button>
+
+                    <button className="flex items-center bg-orange-600 hover:bg-orange-700 text-white font-semibold py-2 px-10 rounded-lg transition duration-300">
+                        <img src="src=/../volume.png" alt="icone_cadastro" className="w-5 h-5 mr-3" />Alarme</button>
+
+                    <button className="flex items-center bg-orange-600 hover:bg-orange-700 text-white font-semibold py-2 px-10 rounded-lg transition duration-300">
+                        <img src="src=/../relatorio.png" alt="icone_cadastro" className="w-5 h-5 mr-3" />Relatório</button>   
+                
+                    <button className="flex items-center bg-orange-600 hover:bg-orange-700 text-white font-semibold py-2 px-10 rounded-lg transition duration-300">
+                        <img src="src=/../dataehora.png" alt="icone_cadastro" className="w-5 h-5 mr-2" />Data e Hora</button>                   
+                </div>
             </div>
             
         
