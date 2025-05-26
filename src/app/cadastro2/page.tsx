6abc4ef9fbbecc832354ddb2b8fdfd2844a71d7f
Fabio Teixeira - EIN - TSI <fabio5413@ica.com>
2025-05-26 16:44:36 -0300

    cadastro2

---
+++ src/app/cadastro2/page.tsx
@@ -0,0 +1,33 @@
+export default function cadastro3(){
+    return(
+        <div className=' flex flex-col justify-center items-center h-screen'>
+            <div className="fixed flex flex-col justify-center items-center h-screen p-0">
+                <img  
+                    src="/display.png" 
+                    alt="display"
+                    className="w-85  h-auto z-99 select-none"></img>
+            </div>
+
+            
+            <div className="fixed inset-0 flex items-center justify-center mt-20 ">
+                <div className=' fixed flex justify-center items-center mb-150 '>
+                    <img src="src=/../veili.png" alt="outralogo" className='mt-100 h-10 z-999 ' />
+                </div>
+                <div className="text-[15px] fixed font-bold mb-55 mr-34">Cadastro</div>
+                <div className="text-[12px] fixed font-bold mb-30">Operador:</div>
+                <img className="fixed h-25 w-25 mt-10" src="src=/../check.png" alt="check" />
+                <div className="text-[12px] fixed font-bold mt-60">Senha cadastrada com sucesso!!!</div>
+
+            </div>
+            
+        
+
+
+
+
+        </div>
+        
+    )
+}
+
+//oi
