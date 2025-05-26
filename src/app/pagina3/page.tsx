00bd4b110e51d6458847e9116ffa73fca3110573
Fabio Teixeira - EIN - TSI <fabio5413@ica.com>
2025-05-26 11:37:44 -0300

    page3

--- src/app/pagina3/page.tsx
+++ src/app/pagina3/page.tsx
@@ -6,8 +6,14 @@ export default function page3(){
                     src="/display.png" 
                     alt="display"
                     className="w-85  h-auto z-99 select-none"></img>
-            </div>   
+            </div>
+                <div className="fixed inset-0 flex items-center justify-center mt-20">
+                <div className=' fixed flex justify-center items-center mb-150 '>
+                    <img src="src=/../veili.png" alt="outralogo" className='mt-100 h-10 z-999' />
+                </div>
+            </div>
         </div>
+        
     )
 }
 
